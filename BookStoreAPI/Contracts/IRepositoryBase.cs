2c85a55dc95e0da5e8adcbde420eb25960fc9058
Božidar Bralić <bozidar.bralic@gmail.com>
2021-12-06 16:54:21 +0100

    Completed repository pattern

---
+++ BookStoreAPI/Contracts/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStoreAPI.Contracts
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        Task<IList<T>> FindAll();
+        Task<T> FindById (int id);
+        Task<bool> Create(T entity);
+        Task<bool> Update(T entity);
+        Task<bool> Delete (T entity);
+        Task<bool> Save();
+    }
+}
---
+++ BookStoreAPI/Services/AuthorRepository.cs
@@ -0,0 +1,52 @@
+﻿using BookStoreAPI.Contracts;
+using BookStoreAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreAPI.Services
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AuthorRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> Create(Author entity)
+        {
+            await _db.Authors.AddAsync(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Delete(Author entity)
+        {
+            _db.Authors.Remove(entity);
+            return await Save();
+        }
+
+        public async Task<IList<Author>> FindAll()
+        {
+            var author = await _db.Authors.ToListAsync();
+            return author;
+        }
+
+        public async Task<Author> FindById(int id)
+        {
+            var obj = await _db.Authors.FindAsync(id);
+            return obj;
+        }
+
+        public async Task<bool> Save()
+        {
+            var changes = await _db.SaveChangesAsync();
+            return changes > 0;
+        }
+
+        public async Task<bool> Update(Author entity)
+        {
+            _db.Authors.Update(entity);
+            return await Save();
+        }
+    }
+}
