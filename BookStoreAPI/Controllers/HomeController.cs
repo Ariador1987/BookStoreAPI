430db5581a397a7a9e79005c2a4ba46038711592
Božidar Bralić <bozidar.bralic@gmail.com>
2021-12-06 15:05:11 +0100

    initial commit

---
+++ BookStoreAPI/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using BookStoreAPI.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStoreAPI.Controllers
+{
+    /// <summary>
+    /// This is an API Test Controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // Dependancy injection
+        private readonly ILoggerService _logger;
+
+        public HomeController(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+
+        
+        /// <returns></returns>
+        // GET: api/<HomeController>
+
+        /// <summary>
+        /// Get Id Value
+        /// </summary>
+        /// <returns></returns>
+        // GET api/<HomeController>/5
+        [HttpGet]
+        public string Get(int id)
+        {
+            _logger.LogInfo("Got info");
+            return "value";
+        }
+
+        // POST api/<HomeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _logger.LogError("Some error");
+        }
+
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HomeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _logger.LogWarn("Warning");
+        }
+    }
+}
---
+++ BookStoreAPI/Program.cs
@@ -0,0 +1,68 @@
+using BookStoreAPI.Contracts;
+using BookStoreAPI.Data;
+using BookStoreAPI.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddSwaggerGen(x =>
+{
+    x.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Book Store API",
+        Version = "v1",
+        Description = "This is an educational API for a Book Store"
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    x.IncludeXmlComments(xmlPath);
+});
+
+builder.Services.AddSingleton<ILoggerService, LoggerService>();
+
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Book Store API");
+    c.RoutePrefix = "";
+});
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
